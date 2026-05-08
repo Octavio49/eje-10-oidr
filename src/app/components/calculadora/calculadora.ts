4141480eec505640d745861191a3a884c4c7dc00
Octavio <deavila.octavio23@gmail.com>
2026-05-08 15:48:32 -0600

    first commit

---
+++ src/app/components/calculadora/calculadora.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [FormsModule],
+  templateUrl: './calculadora.html',
+  styleUrl: './calculadora.css',
+})
+export class Calculadora {
+  resultado=0;
+  n1 = 0;
+  n2 = 0;
+
+  sumar(){
+    this.resultado = this.n1 + this.n2
+  }
+
+  restar(){
+    this.resultado = this.n1 - this.n2
+  }
+}
